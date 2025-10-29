0f91542cad948b6ffa8637c975e59807b029b803
Alfarrel Ersya <alpallel.e@gmail.com>
2025-10-29 13:43:31 +0700

    update quiz answer

--- kelas/Sets, Dictionaries, Randomness/quiz.py
+++ kelas/Sets, Dictionaries, Randomness/quiz.py
@@ -1,13 +1,14 @@
+def reverse_dict(input_dict):
+    hasil = {}
+    for key in input_dict:
+        for val in input_dict[key]:
+            if val not in hasil:
+                hasil.update({val: [key]})
+            if key not in hasil[val]:
+                hasil[val].append(key)
+    return hasil
+
 input_dict = {"a": [1,2,3],
               "b":[2,3],
               "c":[2,3,4,5]}
-
-hasil = {}
-for key in input_dict:
-    for val in input_dict[key]:
-        if val not in hasil:
-            hasil.update({val: [key]})
-        if key not in hasil[val]:
-            hasil[val].append(key)
-
-print(hasil)
+print(reverse_dict(input_dict))
