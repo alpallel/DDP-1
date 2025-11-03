ee421cdfd7911801f97efc06f7e16ceafac7e1ba
Alfarrel Ersya <alpallel.e@gmail.com>
2025-11-03 11:12:03 +0700

    Add recursive functions to determine even and odd numbers

--- kelas/Recursions/!test.py
+++
@@ -1,8 +0,0 @@
-def ispalindrome(inp):
-    def palindrome(inp, n = -1):
-        if n == -len(inp):
-            return inp[0]
-        return inp[n] + palindrome(inp, n-1)
-    return palindrome(inp) == inp
-    
-print(ispalindrome("bammab"))
